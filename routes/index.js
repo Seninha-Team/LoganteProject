32ff3ae8a06b00d418ce1628e27f31df05777720
Thomas Pruner <thomaspruner@hotmail.com>
2025-10-04 02:33:43 -0300

    Acidionaca a variavel global.usuarios no app.js e o router.post do registro no index.js

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,30 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-Feature/SCRUM-14-Rota-get-login
+router.post("/registro", (req, res) => {
+  const { nome, senha } = req.body;
 
+  // validações
+  if (!nome || nome.trim() === "") {
+    return res.status(400).send("Nome não pode ser vazio ou nulo.");
+  }
 
+  if (!senha || senha.trim() === "") {
+    return res.status(400).send("Senha não pode ser vazia ou nula.");
+  }
 
+  // verifica duplicado
+  const usuarioExiste = global.usuarios.some(u => u.nome === nome);
+  if (usuarioExiste) {
+    return res.status(400).send("Usuário já existe.");
+  }
+
+  // adiciona usuário na variável global
+  global.usuarios.push({ nome, senha });
+
+  // renderiza a página deu_certo.ejs passando o nome (MUDAR O NOME DA PÁGINA PARA A QUAL DEVE REDIRECIONAR)
+  res.render("deu_certo", { nome });
+});
 
 router.get('/login', async (req, res, next) => {
   res.render('loginPage');
